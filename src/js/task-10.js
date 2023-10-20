0e7cf4b186f41bca258bae539d30d8a085f526fd
Mishanizm <130248449+Mishanizm@users.noreply.github.com>
2023-10-20 18:32:08 +0100

    go

---
+++ src/js/task-10.js
@@ -0,0 +1,33 @@
+const controlsDiv = document.getElementById("controls");
+const input = controlsDiv.querySelector("input");
+const createButton = controlsDiv.querySelector("[data-create]");
+const destroyButton = controlsDiv.querySelector("[data-destroy]");
+const boxesDiv = document.getElementById("boxes");
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    const size = 30 + i * 10;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
+
+createButton.addEventListener("click", () => {
+  const amount = input.value;
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener("click", destroyBoxes);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
