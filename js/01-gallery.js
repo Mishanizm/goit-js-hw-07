198b694bc9b7fca273a8a9aef25d499d53fdc684
Mishanizm <130248449+Mishanizm@users.noreply.github.com>
2023-10-23 17:20:11 +0100

    пщ

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'; 
+const galleryContainer = document.querySelector('.gallery');
 
-console.log(galleryItems);
+function createGalleryMarkup(items) {
+  return items
+    .map(({ original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
+
+
+galleryContainer.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { target } = e;
+
+  if (target.classList.contains('gallery__image')) {
+    const source = target.getAttribute('href'); 
+    openModal(source);
+  }
+});
+
+function openModal(source) {
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  const closeOnEscape = (e) => {
+    if (e.key === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', closeOnEscape);
+    }
+  };
+
+  window.addEventListener('keydown', closeOnEscape);
+}
