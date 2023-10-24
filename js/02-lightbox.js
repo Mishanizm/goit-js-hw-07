cc2844930a5320f6fc38d042e6e58bdbe4dd2cf2
Mishanizm <130248449+Mishanizm@users.noreply.github.com>
2023-10-24 15:54:30 +0100

    go

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,36 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-import SimpleLightbox from 'simplelightbox'; // Импортируем библиотеку SimpleLightbox
 
 const galleryContainer = document.querySelector('.gallery');
 
-// Создание и рендер разметки по массиву данных
 function createGalleryMarkup(items) {
-    return items
-        .map(({ original, description, preview }) => {
-            return `
-                <li class="gallery__item">
-                    <a class="gallery__link" href="${original}">
-                        <img class="gallery__image" src="${preview}" alt="${description}" />
-                    </a>
-                </li>
-            `;
-        })
-        .join('');
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>
+      `;
+    })
+    .join('');
 }
 
 galleryContainer.innerHTML = createGalleryMarkup(galleryItems);
 
-// Инициализация библиотеки SimpleLightbox
 const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true, // Включаем подписи
-    captionDelay: 250, // Задержка перед появлением подписи (250 миллисекунд)
-    history: false, // Отключаем историю браузера
+  captions: true,
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250, 
+  history: true, 
 });
 
-lightbox.on('show.simplelightbox', function (e) {
-    // Добавляем описание под изображением
-    const description = e.$source.find('img').attr('alt');
-    e.$caption.html(description);
-});
+
